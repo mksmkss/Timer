33c90c44c5ad41f3ff1794cff560e1e1d4a5d0d4
Masataka <masataka2003@gmail.com>
2022-03-03 15:54:22 +0900

    2022/3/3

--- dev/demo.js
+++ dev/demo.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 import { StyleSheet, View,Text } from 'react-native';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
+
 const style = StyleSheet.create( {
     container: {    
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-
+    con: {
+        alignItems: 'center',
+    },
 });
 
 const Demo = () => {
-  const [time, setTime] = React.useState(10);
+  const [time, setTime] = React.useState(20);
   const [isTimerStart, setIsTimerStart] = React.useState(false);
-  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [isPlaying, setIsPlaying] = React.useState(true);
+
     return(
     <View style={style.container}>
        <CountdownCircleTimer
@@ -23,8 +27,13 @@ const Demo = () => {
     duration={time}
     colors={['#004777', '#F7B801', '#A30000', '#A30000']}
     colorsTime={[7, 5, 2, 0]}
+    initialRemainingTime={15}
+    size={200}
   >
-    {({ remainingTime }) => <Text>{remainingTime}</Text>}
+    {({ remainingTime }) => 
+    <View style={style.con}>
+      <Text>{`remaining${remainingTime}`}</Text>
+    </View>}
   </CountdownCircleTimer>
     </View>
     );
